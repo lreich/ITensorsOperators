232892b179429e2e9a31e57bded61c63b31f0020
Lukas Reich <lukasreich@online.de>
2025-10-24 21:29:33 +0200

    Added exports

--- src/ITensorsOperators.jl
+++ src/ITensorsOperators.jl
@@ -1,16 +1,29 @@
 module ITensorsOperators
-#= load external modules =#
-using ITensors, ITensorMPS 
-# using SparseArrays, LinearAlgebra, IterTools, CUDA, JLD2
-
+using ITensors, ITensorMPS, LinearAlgebra
 
 include("BLBQ_model.jl")
+include("TF_Ising_model.jl")
+include("Kitaev_chain.jl")
+include("TFXY_model.jl")
+include("Tight_binding_model.jl")
 
-export decoupled_lattice,
-	   BLBQ_Hamiltonian_MPO,
-	   BLBQ_Hamiltonian_bdg,
-	   string_correlator_MPO,
-	   dimer_phase_order_param,
-	   local_density_density_corr_fct,
-	   cartesianToSpin1
+export BLBQ_Hamiltonian_MPO
+export BLBQ_Hamiltonian_bdg
+export string_correlator_MPO
+export dimer_phase_order_param
+export local_density_density_corr_fct
+export cartesianToSpin1
+
+export TFIM_MPO
+export TFIM_bdg
+
+export kitaev_model
+export kitaev_model_bdg
+
+export TF_XY_Model
+export TF_XY_Model_bdg
+
+export Tight_binding_MPO
+export Tight_binding_bdg
+
 end
